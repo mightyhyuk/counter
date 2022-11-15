ae42631371e99043bfbd4a515b5c68abf18cd9f0
mightyhyuk <118177277+mightyhyuk@users.noreply.github.com>
2022-11-15 13:08:43 +0900

    Create Counter component

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 
+import Counter from "./components/counter";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Counter />
   </React.StrictMode>
 );
 
