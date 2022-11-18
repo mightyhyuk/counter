ad0cb622279eca0286d3c5484ae59195690583fb
mightyhyuk <118177277+mightyhyuk@users.noreply.github.com>
2022-11-18 11:53:26 +0900

    Create Reset button and take the Increment button from Counter

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -14,16 +14,39 @@ class Counters extends Component {
   render() {
     return (
       <>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
         {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
             onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
             counter={counter}
           />
         ))}
       </>
     );
   }
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
 
   handleDelete = (counterId) => {
     const counters = this.state.counters.filter((c) => c.id !== counterId);
