f97b31985c86e66cab95ea6ad728e0b223814994
mightyhyuk <118177277+mightyhyuk@users.noreply.github.com>
2022-11-18 12:49:02 +0900

    Add total counters at Navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,7 +5,12 @@ class Navbar extends Component {
     return (
       <nav className="navbar bg-light">
         <div className="container-fluid">
-          <span className="navbar-brand mb-0 h1">Navbar</span>
+          <span className="navbar-brand mb-0 h1">
+            Navbar{" "}
+            <span className="badge rounded-pill text-bg-secondary">
+              {this.props.totalCounters}
+            </span>
+          </span>
         </div>
       </nav>
     );
