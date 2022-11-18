4c8e39f8be127ff992cc3924f8e2d482d29dbc02
mightyhyuk <118177277+mightyhyuk@users.noreply.github.com>
2022-11-18 12:48:28 +0900

    Lift the state from Counters to App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
+import React, { Component } from "react";
 import "./App.css";
 
 import Navbar from "./components/navbar";
 import Counters from "./components/counters";
 
-function App() {
-  return (
-    <>
-      <Navbar />
-      {/* <header className="App-header"></header> */}
-      <main className="container">
-        <Counters />
-      </main>
-    </>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  render() {
+    return (
+      <>
+        <Navbar
+          totalCounters={this.state.counters.filter((c) => c.value > 0).length}
+        />
+        {/* <header className="App-header"></header> */}
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+          />
+        </main>
+      </>
+    );
+  }
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
 }
 
 export default App;
