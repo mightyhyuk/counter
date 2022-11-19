8b01e4d783a71c5ef6d4ad8aedf1a4b4b4f7b433
mightyhyuk <118177277+mightyhyuk@users.noreply.github.com>
2022-11-19 21:49:47 +0900

    Add Decrement button

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ class App extends Component {
             counters={this.state.counters}
             onReset={this.handleReset}
             onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
             onDelete={this.handleDelete}
           />
         </main>
@@ -46,6 +47,14 @@ class App extends Component {
     const index = counters.indexOf(counter);
     counters[index] = { ...counter };
     counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleDecrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value--;
     this.setState({ counters });
   };
 
