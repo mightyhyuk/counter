c3139c015bc80412500891ac1ca41806582b9116
mightyhyuk <118177277+mightyhyuk@users.noreply.github.com>
2022-11-19 21:10:09 +0900

    Update Navbar to stateless functional component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,20 +1,16 @@
-import React, { Component } from "react";
-
-class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar bg-light">
-        <div className="container-fluid">
-          <span className="navbar-brand mb-0 h1">
-            Navbar{" "}
-            <span className="badge rounded-pill text-bg-secondary">
-              {this.props.totalCounters}
-            </span>
+const Navbar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar bg-light">
+      <div className="container-fluid">
+        <span className="navbar-brand mb-0 h1">
+          Navbar{" "}
+          <span className="badge rounded-pill text-bg-secondary">
+            {totalCounters}
           </span>
-        </div>
-      </nav>
-    );
-  }
-}
+        </span>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
